21f247f91cf5583c37cc02094a94dad9cfcfbb93
MayDan12 <oluwamayowadan@gmail.com>
2024-06-13 13:30:23 +0100

    Added the inspiration component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,98 @@
 import "./App.css";
+import { useState } from "react";
+
+const destinationArtCulture = [
+  { country: "Phoenix", city: "Arizona" },
+  { country: "Hot Springs", city: "Arkansas" },
+  { country: "Los Angeles", city: "California" },
+  { country: "San Diego", city: "California" },
+  { country: "San Fransico", city: "California" },
+  { country: "Barcelona", city: "California" },
+  { country: "Prague", city: "Czechia" },
+  { country: "Washington", city: "District of Columbia" },
+  { country: "Scarborough", city: "England" },
+  { country: "London", city: "England" },
+  { country: "Keswick", city: "England" },
+];
+
+const destinationOutdoorAdventure = [
+  { country: "Paris", city: "France" },
+  { country: "Berlin", city: "Germany" },
+  { country: "Tokyo", city: "Japan" },
+  { country: "Osaka", city: "Japan" },
+  { country: "Kyoto", city: "Japan" },
+  { country: "Madrid", city: "Spain" },
+  { country: "Vienna", city: "Austria" },
+  { country: "Amsterdam", city: "Netherlands" },
+  { country: "Sydney", city: "Australia" },
+  { country: "Melbourne", city: "Australia" },
+  { country: "Brisbane", city: "Australia" },
+];
+
+const mountainCabins = [
+  { country: "Banff", city: "Canada" },
+  { country: "Aspen", city: "USA" },
+  { country: "Zermatt", city: "Switzerland" },
+  { country: "Chamonix", city: "France" },
+  { country: "Innsbruck", city: "Austria" },
+  { country: "Queenstown", city: "New Zealand" },
+  { country: "Nagano", city: "Japan" },
+  { country: "Grindelwald", city: "Switzerland" },
+  { country: "Whistler", city: "Canada" },
+  { country: "Gatlinburg", city: "USA" },
+  { country: "Cortina d'Ampezzo", city: "Italy" },
+];
+
+const beachDestinations = [
+  { country: "Malibu", city: "USA" },
+  { country: "Waikiki", city: "USA" },
+  { country: "Bondi Beach", city: "Australia" },
+  { country: "Phuket", city: "Thailand" },
+  { country: "Bora Bora", city: "French Polynesia" },
+  { country: "Maui", city: "USA" },
+  { country: "Cancun", city: "Mexico" },
+  { country: "Goa", city: "India" },
+  { country: "Nice", city: "France" },
+  { country: "Barceloneta", city: "Spain" },
+  { country: "Copacabana", city: "Brazil" },
+];
+
+const popularDestinations = [
+  { country: "New York", city: "USA" },
+  { country: "Paris", city: "France" },
+  { country: "Rome", city: "Italy" },
+  { country: "London", city: "England" },
+  { country: "Tokyo", city: "Japan" },
+  { country: "Dubai", city: "United Arab Emirates" },
+  { country: "Istanbul", city: "Turkey" },
+  { country: "Bangkok", city: "Thailand" },
+  { country: "Barcelona", city: "Spain" },
+  { country: "Singapore", city: "Singapore" },
+  { country: "Sydney", city: "Australia" },
+];
+
+const uniqueStays = [
+  { country: "Reykjavik", city: "Iceland" },
+  { country: "Petra", city: "Jordan" },
+  { country: "Marrakech", city: "Morocco" },
+  { country: "Hobbiton", city: "New Zealand" },
+  { country: "Santorini", city: "Greece" },
+  { country: "Machu Picchu", city: "Peru" },
+  { country: "Cusco", city: "Peru" },
+  { country: "Kyoto", city: "Japan" },
+  { country: "Queenstown", city: "New Zealand" },
+  { country: "Salar de Uyuni", city: "Bolivia" },
+  { country: "Lapland", city: "Finland" },
+];
+
+const hotelImg = [
+  { img: "./images/hotel.png" },
+  { img: "./images/hotel2.png" },
+  { img: "./images/hotel3.png" },
+  { img: "./images/hotel4.png" },
+  { img: "./images/hotel5.png" },
+  { img: "./images/hotel6.png" },
+];
 
 const Hotels = {
   id: crypto.randomUUID(),
@@ -6,8 +100,8 @@ const Hotels = {
   name: "Bangkok & Pattaya",
   desc: "Airfare, 2 Nights Bangkok, 3 Nights Pattaya, Breakfast, Bangkok City Tour, Coral Island Tour, Underwater World tour Pattaya, Transfers & All Taxes",
   valid: "10th Dec 2022",
-  price: "₹34,999",
-  slashedPrice: "₹32,999",
+  price: 34999,
+  slashedPrice: 32999,
   discount: true,
 };
 export default function App() {
@@ -18,6 +112,7 @@ export default function App() {
       <PopularPackages />
       <PackagesList />
       <InspirationPage />
+      <Footer />
     </>
   );
 }
@@ -78,30 +173,157 @@ function PackagesList() {
 
 function Package() {
   return (
-    <div className="package">
+    <div className="pack-img">
       <div>
         <img src={Hotels.image} alt="Hotel" />
       </div>
-      <div>
+      <div className="package">
         <h1>{Hotels.name}</h1>
         <p>{Hotels.desc}</p>
         <p>Travel Vallid Till: {Hotels.valid}</p>
         <p>
-          Starting From <span>{Hotels.price}</span> {Hotels.slashedPrice}
+          Starting From <span>₹{Hotels.price}</span> ₹{Hotels.slashedPrice}
         </p>
         <p></p>
         <p>{Hotels.discount}</p>
       </div>
-      <button>Book Now</button>
+      <div className="btn">
+        <button>Book Now</button>
+      </div>
     </div>
   );
 }
 
 function InspirationPage() {
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
     <div className="container inspiration">
-      <h1>Inspiration for future getaways packages</h1>
-      <div></div>
+      <h1>Inspiration for Future Getaways Packages</h1>
+      <div className="inspiration-tab">
+        <TabButton index={0} activeTab={activeTab} setActiveTab={setActiveTab}>
+          Destinations for arts & culture
+        </TabButton>
+        <TabButton index={1} activeTab={activeTab} setActiveTab={setActiveTab}>
+          Destinations for outdoor adventure
+        </TabButton>
+        <TabButton index={2} activeTab={activeTab} setActiveTab={setActiveTab}>
+          Mountain cabins
+        </TabButton>
+        <TabButton index={3} activeTab={activeTab} setActiveTab={setActiveTab}>
+          Beach destinations
+        </TabButton>
+        <TabButton index={4} activeTab={activeTab} setActiveTab={setActiveTab}>
+          Popular destinations
+        </TabButton>
+        <TabButton index={5} activeTab={activeTab} setActiveTab={setActiveTab}>
+          Unique Stays
+        </TabButton>
+      </div>
+
+      <div className="inspiration-content">
+        {activeTab === 0 && <DestintionArtCulture />}
+        {activeTab === 1 && <DestinationOutdoorAdventure />}
+        {activeTab === 2 && <MountainCabins />}
+        {activeTab === 3 && <BeachDestinations />}
+        {activeTab === 4 && <PopularDestinations />}
+        {activeTab === 5 && <UniqueStays />}
+      </div>
     </div>
+  );
+}
+
+function TabButton({ index, activeTab, setActiveTab, children }) {
+  return (
+    <div>
+      <span
+        onClick={() => setActiveTab(index)}
+        href="https://"
+        className={`insp-btn ${activeTab === index ? "active" : ""}`}
+      >
+        {children}
+      </span>
+    </div>
+  );
+}
+
+function Destinations({ children }) {
+  return (
+    <div>
+      <div className="dest">
+        {children.map((dest) => (
+          <Destination key={dest.city} dest={dest} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Destination({ dest }) {
+  return (
+    <div className="box">
+      <h2>{dest.country}</h2>
+      <p>{dest.city}</p>
+    </div>
+  );
+}
+
+function DestintionArtCulture() {
+  return (
+    <div>
+      <Destinations children={destinationArtCulture} />
+    </div>
+  );
+}
+
+function DestinationOutdoorAdventure() {
+  return (
+    <div>
+      <Destinations children={destinationOutdoorAdventure} />
+    </div>
+  );
+}
+
+function MountainCabins() {
+  return (
+    <div>
+      <Destinations children={mountainCabins} />
+    </div>
+  );
+}
+
+function BeachDestinations() {
+  return (
+    <div>
+      <Destinations children={beachDestinations} />
+    </div>
+  );
+}
+
+function PopularDestinations() {
+  return (
+    <div>
+      <Destinations children={popularDestinations} />
+    </div>
+  );
+}
+
+function UniqueStays() {
+  return (
+    <div>
+      <Destinations children={uniqueStays} />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="container">
+      <div className="footer-box">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </footer>
   );
 }
